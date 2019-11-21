d2e6058e8dbfa13c6fe0d7c46387904842339884
Renata Ashapatov <renataash15@gmail.com>
2019-11-20 19:24:11 -0500

    My third CS homework!

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-
+from statistics import mean
 
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
@@ -7,8 +7,16 @@ def main():
     input_file_path = sys.argv[1]
     print(f"Processing input file: {input_file_path}")
 
-    # TODO: Fill in the actual logic here!
 
+    a = open(input_file_path, "r")
+    for line in a:
+        x = line.split(',')
+        numbers = []
+        for n in x:
+            numbers.append(float(n))
+        avg = mean(numbers)
+        print(avg)
 
 if __name__ == "__main__":
     main()
+
